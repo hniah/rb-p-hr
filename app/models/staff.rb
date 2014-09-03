6219b7f44860190d010cf0c699903828f8c9bf3f
Martin <vuquangthang87@gmail.com>
2014-09-03 09:34:09 +0700

    Remove #cumulative_leaves_in

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -13,19 +13,7 @@ class Staff < User
   validates :lates, length: {maximum: 10, message: 'Staff cannot have more than 10 leaves. Send him a warning letter.'}
 
   def remaining_leave_days_in(current_year)
-    started_year = self.started_on.year
-    cumulative_leaves = 0.0
-    cumulative_leaves = cumulative_leaves_in(started_year, current_year - 1) if started_year < current_year
-    LEAVE_DAYS + cumulative_leaves - total_leave_days_in(current_year)
-  end
-
-  def cumulative_leaves_in(started_year, year)
-    total = 0.0
-    started_year.upto(year).each do |y|
-      c = total_leave_days_in(y)
-      total = [MAX_CUMULATIVE_LEAVE_DAYS, LEAVE_DAYS + total - c].min
-    end
-    total
+    LEAVE_DAYS - total_leave_days_in(current_year)
   end
 
   def total_leave_days_in(year)
--- spec/models/staff_spec.rb
+++ spec/models/staff_spec.rb
@@ -34,19 +34,6 @@ describe Staff do
     end
   end
 
-  describe '#cumulative_leaves' do
-    let(:staff) { create :staff }
-    let(:leave_days_last_year) { create_list :leave_day, 3, date: '2013-08-09' }
-    let(:leave_last_year) { create :leave, status: :approved, leave_days: leave_days_last_year }
-
-    before { create :leave, status: :approved, staff: staff, leave_days: create_list(:leave_day, 14, date: '2012-03-02') }
-    before { staff.leaves << leave_last_year }
-
-    it 'calculates total leave days in this year of the staff' do
-      expect(staff.cumulative_leaves_in(staff.started_on.year, 2013)).to eq 7
-    end
-  end
-
   describe '#remaining_leave_days' do
     let(:staff) { create :staff, started_on: '2012-02-02' }
     let(:leave_days_this_year) { create_list :leave_day, 3, date: Date.today }
@@ -60,7 +47,7 @@ describe Staff do
     before { staff.leaves << leave_last_year }
 
     it 'calculates total leave days in this year of the staff' do
-      expect(staff.remaining_leave_days_in(Time.now.year)).to eq 13.5
+      expect(staff.remaining_leave_days_in(Time.now.year)).to eq 6.5
     end
   end
 
