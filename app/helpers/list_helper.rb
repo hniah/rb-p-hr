0a304a0481ff85ae61df6e9d586da2539e6fc6f0
Martin <vuquangthang87@gmail.com>
2014-09-04 09:50:52 +0700

    Refactor #list_button in ListHelper module

--- app/helpers/list_helper.rb
+++ app/helpers/list_helper.rb
@@ -1,43 +1,17 @@
 module ListHelper
-  def list_edit_button(resource, type)
-    link_to 'Edit', list_action_path(resource, :edit, type), list_edit_button_options(resource)
-  end
 
-  def list_delete_button(resource, type)
-    link_to 'Delete', list_action_path(resource, :delete, type), list_delete_button_options(resource)
-  end
-
-  def list_detail_button(resource, type)
-    link_to 'Detail', list_action_path(resource, :detail, type), list_detail_button_options(resource)
-  end
-
-  def list_action_path(resource, action, type)
-    if type == 'frontend'
-      if action == :edit
-        path = "edit_#{resource_class(resource)}_path".to_sym
-      elsif action == :delete
-        path = "#{resource_class(resource)}_path".to_sym
-      elsif action == :detail
-        path = "#{resource_class(resource)}_path".to_sym
-      end
-    elsif type == 'admin'
-      if action == :edit
-        path = "edit_admin_#{resource_class(resource)}_path".to_sym
-      elsif action == :delete
-        path = "admin_#{resource_class(resource)}_path".to_sym
-      elsif action == :detail
-        path = "admin_#{resource_class(resource)}_path".to_sym
-      end
+  def list_button(name:, action:, admin:, resource:)
+    if action == :edit
+      link_to name, [:edit, admin, resource], list_edit_button_options(resource)
+    elsif action == :delete
+      link_to name, [admin, resource], list_delete_button_options(resource)
+    elsif action == :detail
+      link_to name, [admin, resource], list_detail_button_options(resource)
     end
-    send(path, resource.id)
   end
 
   def resource_class(resource)
     resource.class.to_s.downcase
-  end
-
-  def list_detail_button_options(resource)
-    {class: 'list-edit-button', data: {test: "view-#{resource_class(resource)}-#{resource.id}"}}
   end
 
   def list_edit_button_options(resource)
@@ -46,6 +20,10 @@ module ListHelper
 
   def list_delete_button_options(resource)
     {method: :delete, class: 'list-delete-button', data:{confirm: t("#{resource_class(resource)}.message.delete_confirmation"), test: "delete-#{resource_class(resource)}-#{resource.id}"}}
+  end
+
+  def list_detail_button_options(resource)
+    {method: :get, class: 'list-edit-button', data: {test: "view-#{resource_class(resource)}-#{resource.id}"}}
   end
 
   def bootstrap_paginate(resources)
