f89abca758e6d21b7aa213909a7a9c8478aebcb0
Martin <vuquangthang87@gmail.com>
2014-09-10 14:57:04 +0700

    Update Leaves Migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140909020754) do
+ActiveRecord::Schema.define(version: 20140910035404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,10 @@ ActiveRecord::Schema.define(version: 20140909020754) do
     t.integer  "staff_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.date     "start"
+    t.date     "end"
+    t.string   "sub_cate"
+    t.integer  "count"
   end
 
   create_table "settings", force: true do |t|
