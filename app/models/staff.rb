095742318b8b5df048d86fceca653b871f9b32f9
Martin <vuquangthang87@gmail.com>
2014-09-16 17:12:44 +0700

    Complete #remaining_leave_days for Staff

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -2,14 +2,15 @@ class Staff < User
 
   scope :to_options, -> { all.collect { |staff| [ staff.english_name, staff.id ] } }
 
-  LEAVE_DAYS = 14.0
-  MAX_CUMULATIVE_LEAVE_DAYS = 7.0
-
   has_many :leaves
   has_many :lates
   has_many :feedbacks
   has_many :leave_days, through: :leaves
 
   validates :lates, length: {maximum: 10, message: 'Staff cannot have more than 10 leaves. Send him a warning letter.'}
+
+  def remaining_leave_days
+    self.leaves.current_year.approved.sum(:total)
+  end
 
 end
--- spec/models/staff_spec.rb
+++ spec/models/staff_spec.rb
@@ -17,5 +17,17 @@ describe Staff do
       expect(jack).to_not be_valid
     end
   end
-end
 
+  describe '#remaining_leave_days' do
+    let!(:martin) { create :staff }
+    let!(:total_leave_in_year) { create :leave, start_day: '2014-01-01 8:30', end_day: '2014-12-31 17:30', total: 14, staff: martin}
+    let!(:leaves_current_year) { create_list :leave, 2, start_day: '2014-09-16 8:30', end_day: '2014-09-18 17:30', total: -3.0, staff: martin}
+    let!(:leaves_last_year) { create_list :leave, 2, start_day: '2013-09-16 8:30', end_day: '2013-09-18 17:30', total: -3.0, staff: martin}
+
+    context 'calculator remaining leave days for staff' do
+      it 'calculator remaining leave days for staff' do
+        expect(martin.remaining_leave_days).to eq 8
+      end
+    end
+  end
+end
