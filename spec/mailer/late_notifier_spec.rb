5d6bdbfb8d0e79ac22e8ba698910061841b70b38
Martin <vuquangthang87@gmail.com>
2014-09-22 09:02:25 +0700

    Update EMAIL_NOTIFIER value for rspec

--- spec/mailer/late_notifier_spec.rb
+++ spec/mailer/late_notifier_spec.rb
@@ -4,7 +4,7 @@ describe LateNotifier do
   describe '.more_late' do
     let(:staff) { create :staff }
     let(:late) { create(:late , staff: staff) }
-    let!(:EMAIL_NOTIFIER) { create :setting, key: 'EMAIL_NOTIFIER', value: 'martin@futureworkz.com' }
+    let!(:EMAIL_NOTIFIER) { create :setting, key: 'EMAIL_NOTIFIER', value: 'jack@futureworkz.com' }
 
     before { ActionMailer::Base.deliveries = [] }
 
@@ -12,7 +12,7 @@ describe LateNotifier do
       LateNotifier.more_late(late).deliver
       expect(ActionMailer::Base.deliveries.size).to eq 1
       expect(ActionMailer::Base.deliveries.first.subject).to include('Staff has more than 10 lates this year')
-      expect(ActionMailer::Base.deliveries.first.to).to include 'martin@futureworkz.com'
+      expect(ActionMailer::Base.deliveries.first.to).to include 'jack@futureworkz.com'
     end
   end
 end
