85d578366392857e1ffa6b15b8c3208f98bc727c
Martin <vuquangthang87@gmail.com>
2014-09-23 15:17:21 +0700

    Add check new year to add 14 leave days for staff

--- app/services/leave_days.rb
+++ app/services/leave_days.rb
@@ -1,17 +1,29 @@
 class LeaveDays
   def self.add_leave_days_staff
-    self.staffs.each do |staff|
-      @leave = Leave.find_or_initialize_by(self.leave_param(staff.id))
-      @leave.save
+    if LeaveDays.is_new_year?
+      self.staffs.each do |staff|
+        self.add_annual_leave_days(staff)
+      end
     end
+  end
+
+  protected
+  def self.is_new_year?
+    Date.today.beginning_of_year == Date.today
   end
 
   private
   def self.leave_param(staff_id)
-    { reason: 'Add Leave days for new year', status: :approved, staff_id: staff_id, category: :annual, start_day: "01-01-#{Time.now.year} 08:30".to_date, end_day: "31-12-#{Time.now.year} 17:30".to_date, total: 14, sub_cate: 'carry-over leaves'}
+    { reason: 'Add Leave days for new year', status: :approved, staff_id: staff_id, category: :annual, start_day: Date.today.beginning_of_year, end_day: Date.today.end_of_year, total: 14, sub_cate: 'carry-over leaves'}
   end
 
   def self.staffs
     Staff.all
   end
+
+  def self.add_annual_leave_days(staff)
+    @leave = Leave.find_or_initialize_by(self.leave_param(staff.id))
+    @leave.save
+  end
+
 end
--- spec/services/leave_days_spec.rb
+++ spec/services/leave_days_spec.rb
@@ -3,12 +3,17 @@ require 'rails_helper'
 describe 'Add 14 leave days for staff when new year comes' do
   let!(:staffs) { create_list :staff, 4 }
 
-  before { LeaveDays.add_leave_days_staff }
+  before do
+    allow(Date).to receive(:today).and_return(Date.today.beginning_of_year)
+    LeaveDays.add_leave_days_staff
+  end
+
   context 'Add 14 leave days for staff when new year comes' do
     it 'Add 14 leave days for staff when new year comes' do
+
       expect(Leave.count).to eq 4
-      expect(Leave.first.start_day.strftime('%d/%m/%Y')).to eq "01/01/#{Time.now.year}"
-      expect(Leave.first.end_day.strftime('%d/%m/%Y')).to eq "31/12/#{Time.now.year}"
+      expect(I18n.l(Leave.first.start_day)).to eq "01/01/#{Time.now.year}"
+      expect(I18n.l(Leave.first.end_day)).to eq "31/12/#{Time.now.year}"
     end
   end
 end
