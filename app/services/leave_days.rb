5f50b974277de9a54a325b4822682b6d12b2b3f7
Martin <vuquangthang87@gmail.com>
2014-09-24 16:50:39 +0700

    Add calculate cumulative leave days for staff in rake task add_leave_days_staff

--- app/services/leave_days.rb
+++ app/services/leave_days.rb
@@ -1,9 +1,8 @@
 class LeaveDays
   def self.add_leave_days_staff
     if LeaveDays.is_new_year?
-      self.staffs.each do |staff|
-        self.add_annual_leave_days(staff)
-      end
+      self.add_annual_leave_days
+      self.add_cumulative_leave_days
     end
   end
 
@@ -13,17 +12,28 @@ class LeaveDays
   end
 
   private
-  def self.leave_param(staff_id)
-    { reason: 'Add Leave days for new year', status: :approved, staff_id: staff_id, category: :annual, start_day: Date.today.beginning_of_year, end_day: Date.today.end_of_year, total: 14, sub_cate: 'carry-over leaves'}
+  def self.leave_param(staff_id, total, sub_cate)
+    { reason: 'Add Leave days for new year', status: :approved, staff_id: staff_id, category: :annual, start_day: Date.today.beginning_of_year, end_day: Date.today.end_of_year, total: total, sub_cate: sub_cate}
   end
 
   def self.staffs
     Staff.all
   end
 
-  def self.add_annual_leave_days(staff)
-    @leave = Leave.find_or_initialize_by(self.leave_param(staff.id))
-    @leave.save
+  def self.add_annual_leave_days
+    self.staffs.each do |staff|
+      @leave = Leave.find_or_initialize_by(self.leave_param(staff.id, 14, 'carry-over leaves'))
+      @leave.save
+    end
+  end
+
+  def self.add_cumulative_leave_days
+    self.staffs.each do |staff|
+      cumulative_days = staff.remaining_leave_days(Time.now.year - 1)
+      cumulative_days = 7 if cumulative_days > 7
+      @leave = Leave.find_or_initialize_by(self.leave_param(staff.id, cumulative_days, 'cumulative leaves'))
+      @leave.save
+    end
   end
 
 end
--- spec/services/leave_days_spec.rb
+++ spec/services/leave_days_spec.rb
@@ -1,19 +1,43 @@
 require 'rails_helper'
 
-describe 'Add 14 leave days for staff when new year comes' do
-  let!(:staffs) { create_list :staff, 4 }
+describe LeaveDays do
+  describe 'Add 14 leave days for staff when new year comes' do
+    let!(:staffs) { create_list :staff, 2 }
 
-  before do
-    allow(Date).to receive(:today).and_return(Date.today.beginning_of_year)
-    LeaveDays.add_leave_days_staff
+    before do
+      allow(Date).to receive(:today).and_return(Date.today.beginning_of_year)
+      LeaveDays.add_leave_days_staff
+    end
+
+    context 'Add 14 leave days for staff when new year comes' do
+      it 'Add 14 leave days for staff when new year comes' do
+        expect(Leave.count).to eq 4
+        expect(I18n.l(Leave.last.start_day)).to eq "01/01/#{Time.now.year}"
+        expect(I18n.l(Leave.last.end_day)).to eq "31/12/#{Time.now.year}"
+        expect(Leave.last.sub_cate).to eq 'carry-over leaves'
+      end
+    end
   end
 
-  context 'Add 14 leave days for staff when new year comes' do
-    it 'Add 14 leave days for staff when new year comes' do
+  describe '' do
+    let!(:staffs) { create_list :staff, 2 }
+    let!(:martin) { create :staff }
+    let!(:total_leave_in_last_year) { create :leave, category: :annual, status: :approved, start_day: '2013-01-01 8:30', end_day: '2013-12-31 17:30', total: 14, staff: martin}
+    let!(:leave_days_in_last_year_martin) { create_list :leave, 3, staff: martin, status: :approved, category: :annual, start_day: '2013/10/10', end_day: '2013/10/10', total: -1.0 }
 
-      expect(Leave.count).to eq 4
-      expect(I18n.l(Leave.first.start_day)).to eq "01/01/#{Time.now.year}"
-      expect(I18n.l(Leave.first.end_day)).to eq "31/12/#{Time.now.year}"
+    before do
+      allow(Date).to receive(:today).and_return(Date.today.beginning_of_year)
+      martin.leaves << total_leave_in_last_year
+      martin.leaves << leave_days_in_last_year_martin
+      LeaveDays.add_leave_days_staff
+    end
+
+    context 'Add cumulative leave days for staff when new year comes' do
+      it 'Add cumulative leave days for staff when new year comes' do
+        expect(martin.leaves.count).to eq 6
+        expect(martin.leaves.first.total).to eq 7
+        expect(martin.leaves.first.sub_cate).to eq 'cumulative leaves'
+      end
     end
   end
 end
