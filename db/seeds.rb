f18ef750a344c3378a75dab55492a277121959b7
Jack A. Huang <huynhquancam@gmail.com>
2014-12-12 13:24:40 +0700

    Remove dangerous seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,1 @@
-puts '--- Start seeding sick leaves for all staff'
-Staff.all.each do |staff|
-  staff.leaves.new(
-    category: :sick, 
-    total: 7,
-    reason: 'Add 7 sick leave',
-    status: :approved,
-    start_day: Date.today.change(month: 2),
-    end_day: Date.today.change(month: 2)
-  ).save!
-end
+puts 'No seeding for now'
