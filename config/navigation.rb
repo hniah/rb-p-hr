c097344bd2a93d65c726f934c2fcae976387460d
Jack A. Huang <huynhquancam@gmail.com>
2014-12-27 17:13:05 +0700

    Update admin side menu

--- config/navigation.rb
+++ config/navigation.rb
@@ -6,32 +6,32 @@ SimpleNavigation::Configuration.run do |navigation|
 
     primary.item :home, fa_icon('home', text: '<span>Home</span>'.html_safe), root_path, if: -> { !current_user.is_admin? }
 
-    primary.item :leaves, fa_icon('calendar', text: '<span>Leaves</span>'.html_safe), '#', class: 'has-submenu', if: -> { user_signed_in? && !current_user.is_admin? } do |submenu|
+    primary.item :leaves, fa_icon('calendar', text: '<span>Leaves</span>'.html_safe), '#', class: 'has-submenu', if: -> { !current_user.is_admin? } do |submenu|
       submenu.dom_class = 'submenu'
       submenu.item :index, 'Leaves List', staff_leaves_path
       submenu.item :new, 'Submit a leave', new_staff_leave_path
     end
 
-    primary.item :leaves, 'Leave', admin_leaves_path, if: -> { current_user.is_admin? } do |submenu|
-      submenu.dom_class = 'sub-menu'
-      submenu.item :index, 'Leaves List', admin_leaves_path
-      submenu.item :new, 'Add New Leave', new_admin_leave_path
-    end
-
-    primary.item :users, 'Staff', admin_staffs_path, if: -> { user_signed_in? && current_user.is_admin? } do |submenu|
-      submenu.dom_class = 'sub-menu'
+    primary.item :users, fa_icon('users', text: '<span>Staff</span>'.html_safe), '#', class: 'has-submenu', if: -> { user_signed_in? && current_user.is_admin? } do |submenu|
+      submenu.dom_class = 'submenu'
       submenu.item :index, 'Staff List', admin_staffs_path
       submenu.item :new, 'Add New Staff', new_admin_staff_path
     end
 
-    primary.item :lates, 'Late', admin_lates_path, if: -> { user_signed_in? && current_user.is_admin? } do |submenu|
-      submenu.dom_class = 'sub-menu'
+    primary.item :leaves, fa_icon('calendar', text: '<span>Leave</span<'.html_safe), '#', class: 'has-submenu', if: -> { current_user.is_admin? } do |submenu|
+      submenu.dom_class = 'submenu'
+      submenu.item :index, 'Leaves List', admin_leaves_path
+      submenu.item :new, 'Add New Leave', new_admin_leave_path
+    end
+
+    primary.item :lates, fa_icon('clock-o', text: '<span>Late</span>'.html_safe), '#', if: -> { current_user.is_admin? } do |submenu|
+      submenu.dom_class = 'submenu'
       submenu.item :index, 'Late List', admin_lates_path
       submenu.item :new, 'Add New Late', new_admin_late_path
     end
 
-    primary.item :feedbacks, 'Feedbacks List', admin_feedbacks_path, if: -> { user_signed_in? && current_user.is_admin? }
-    primary.item :settings, 'Setting', admin_settings_path, if: -> { user_signed_in? && current_user.is_admin? }
+    primary.item :feedbacks, fa_icon('envelope', text: '<span>Feedback</span>'.html_safe), admin_feedbacks_path, if: -> { user_signed_in? && current_user.is_admin? }
+    primary.item :settings, fa_icon('cog', text: '<span>Setting</span>'.html_safe), admin_settings_path, if: -> { user_signed_in? && current_user.is_admin? }
     primary.item :pcc, fa_icon('info', text: '<span>PCC</span>'.html_safe), pcc_path
   end
 end
