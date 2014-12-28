aa147a61004903fc0466d5df0af4b14b5cdb18bb
Jack A. Huang <huynhquancam@gmail.com>
2014-12-28 22:10:55 +0700

    Add Form UI css

--- app/controllers/logged_in_controller.rb
+++ app/controllers/logged_in_controller.rb
@@ -1,7 +1,4 @@
-class LoggedInController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+class LoggedInController < ApplicationController
   before_filter :authenticate_user!
   layout 'logged_in'
 
